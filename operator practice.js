08aa41830129b02eeb81067bbfcaaf1a31d7329c
Giri-10-11-2005 <giridharantamil2005@gmail.com>
2025-06-19 15:57:33 +0530

    first

---
+++ operator practice.js
@@ -0,0 +1,20 @@
+let a=100;
+let b=37;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);  
+console.log(a%b);
+console.log(a**b);
+console.log(a++); // Post-increment
+console.log(++a); // Pre-increment 
+console.log(a--); // Post-decrement
+console.log(--a); // Pre-decrement
+console.log(a); // Final value of a after all operations
+let name="GIRIDHARAN"
+if(a > 5) {
+    console.log("a is greater than 5");
+}
+else{
+    console.log("a is not greater than 5");
+}
---
+++ practice.js
@@ -0,0 +1,7 @@
+let name="GIRIDHARAN"
+let a=10;
+let isbool=true;
+let c;
+let b=null;
+let abc=2374638778463274;
+console.log(name, a, isbool, c, b, abc);
